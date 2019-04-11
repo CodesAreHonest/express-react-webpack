ab3149bca9584d9c03cb999f45b4d945cb7a1430
YingHua Chai <yinghuachai0@gmail.com>
2019-04-11 11:21:51 +0800

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "plugins": [ "react" ],
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+    ],
+    "parser": "babel-eslint",
+    "rules": {
+        "no-console": 0,
+        "no-undef": 0
+    }
+  };
